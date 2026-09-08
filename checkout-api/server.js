9dcd7ed7f8bb2398a82a88037bd47c263c812ee5
Kabazbay <kabazbay98@gmail.com>
2026-09-08 01:32:26 +0100

    test: force process exit on startup

--- checkout-api/server.js
+++ checkout-api/server.js
@@ -1,10 +1,10 @@
-// This will crash Node on startup
-const missing = require('nonexistent-broken-package');
-
 const express = require('express');
 const { Pool } = require('pg');
 
 const app = express();
+
+// Force immediate exit on startup
+process.exit(1);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
