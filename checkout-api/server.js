cb3adba6dade5f933d3557b361b8f699a7fd1eac
Kabazbay <kabazbay98@gmail.com>
2026-09-08 01:29:20 +0100

    test: require missing package to break startup

--- checkout-api/server.js
+++ checkout-api/server.js
@@ -1,10 +1,10 @@
+// This will crash Node on startup
+const missing = require('nonexistent-broken-package');
+
 const express = require('express');
 const { Pool } = require('pg');
 
 const app = express();
-
-// Intentional syntax error
-const broken = {
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
