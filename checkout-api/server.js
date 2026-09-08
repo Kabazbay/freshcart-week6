a9ef7da4280e15c43345526429b5d0d9420a9453
Kabazbay <kabazbay98@gmail.com>
2026-09-08 01:26:26 +0100

    test: intentional syntax error for rollback test

--- checkout-api/server.js
+++ checkout-api/server.js
@@ -1,3 +1,11 @@
-app.get("/api/products", (req, res) => {
-  throw new Error("Intentional crash");
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+
+// Intentional syntax error
+const broken = {
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
